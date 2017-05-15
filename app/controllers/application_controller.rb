38e8405559c1bd6f6a70fed481a3e819bbc7d270
Chiwen Chen <chiwenchen@deMac-mini.local>
2017-05-15 14:05:44 +0800

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :setting_locale
 
   def after_sign_in_path_for(resource)
     if resource.admin?
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     else
       request.env['omniauth.origin'] || landing_home_index_path
     end
+  end
+
+  def setting_locale
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
