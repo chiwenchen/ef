87f96b29f3cf2af24e387363ac7b05094c615696
Chiwen Chen <chiwenchen@deMac-mini.local>
2017-07-20 17:17:32 +0800

    multi lang and mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => Setting.domain }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mandrillapp.com',
+    # address:              'smtp.mandrillapp.com',
+    # port:                 587,
+    # user_name:            Setting.mandrill_user_name,
+    # password:             Setting.mandrill_api_key,
+    # authentication:       'plain',
+    # enable_starttls_auto: true
+    address: 'smtp.mailgun.org',
     port:                 587,
-    user_name:            Setting.mandrill_user_name,
-    password:             Setting.mandrill_api_key,
+    user_name: 'cwchen2000@gmail.com',
+    domain: 'sandbox4124b25a8f214c8892346bb9e6721c06.mailgun.org',
+    password: 'pubkey-fad9fbd7d2149c07fe083bbe37516cab',
     authentication:       'plain',
     enable_starttls_auto: true
   }
