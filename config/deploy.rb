28eb5958348d3c382c1dacb7e490408c078c446c
Chiwen Chen <cwchen2000@gmail.com>
2017-11-07 01:16:38 +0800

    change version

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 # set :rvm_type, :user
-# set :rvm_ruby_version, '2.2.5'
+set :rvm_ruby_version, '2.4.1'
 # set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 # SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
 
