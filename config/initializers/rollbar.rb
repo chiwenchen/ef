68015fe3c70ddcfe35a781bb7d58215000755d1b
Chiwen Chen <cwchen2000@gmail.com>
2017-11-27 12:19:59 +0800

    remove rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,5 @@
-require 'rollbar'
+# require 'rollbar'
 
-Rollbar.configure do |config|
-  config.access_token = 'b07393a304c241d3b27d58c8c6d8214c'
-end
+# Rollbar.configure do |config|
+#   config.access_token = 'b07393a304c241d3b27d58c8c6d8214c'
+# end
